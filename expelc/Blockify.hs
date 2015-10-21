39205c94d735a4c66e3ca20605b077cc69a845df
Haldean Brown <will.h.brown@gmail.com>
2015-10-21 07:45:18 -0700

    add mergeBlocks to block parser

--- expelc/Blockify.hs
+++ expelc/Blockify.hs
@@ -8,8 +8,21 @@ module Blockify where
                      , contents :: T.Text
                      } deriving (Show)
 
+  mergeBlocks :: [Block] -> [Block]
+  mergeBlocks = foldr
+    (\b acc -> if null acc then [b] else let prev = head acc in
+      if indent b == indent prev
+        then Block {
+          startLine = startLine b,
+          endLine = endLine prev,
+          indent = indent b,
+          contents = T.intercalate (T.pack "\n") [contents b, contents prev]
+          } : tail acc
+        else b : acc)
+    []
+
   toBlocks :: T.Text -> [Block]
-  toBlocks t = zipWith lineToBlock [0..] (T.lines t)
+  toBlocks t = mergeBlocks $ zipWith lineToBlock [0..] (T.lines t)
     where
       lineToBlock i l =
         let stripped = T.dropWhile isSpace l
