6b882ee577d779d9db20f44742089718df8ce852
Haldean Brown <will.h.brown@gmail.com>
2015-10-25 23:53:31 -0700

    add children to blocks, create children based on indentation

--- expelc/Blockify.hs
+++ expelc/Blockify.hs
@@ -6,23 +6,50 @@ module Blockify where
                      , endLine :: Int
                      , indent :: Int
                      , contents :: T.Text
-                     } deriving (Show)
+                     , children :: [Block]
+                     }
 
-  mergeBlocks :: [Block] -> [Block]
-  mergeBlocks = foldr
+  instance Show Block where
+    show b = replicate (indent b) ' ' ++ show (startLine b) ++ " " ++
+      show (contents b) ++ " {\n" ++
+      T.unpack (T.intercalate (T.pack "\n") (map (T.pack . show) (children b)))
+      ++ replicate (indent b) ' ' ++ "} " ++ show (endLine b) ++ "\n"
+
+  mergeSameLevel :: [Block] -> [Block]
+  mergeSameLevel = foldr
     (\b acc -> if null acc then [b] else let prev = head acc in
       if indent b == indent prev
         then Block {
           startLine = startLine b,
           endLine = endLine prev,
           indent = indent b,
-          contents = T.intercalate (T.pack "\n") [contents b, contents prev]
+          contents = T.intercalate (T.pack "\n") [contents b, contents prev],
+          children = []
           } : tail acc
         else b : acc)
     []
 
-  toBlocks :: T.Text -> [Block]
-  toBlocks t = mergeBlocks $ zipWith lineToBlock [0..] (T.lines t)
+  assignChildren' :: [Block] -> [Block]
+  assignChildren' (x:xs) =
+    x { children = takeWhile isChild xs} : assignChildren' (dropWhile isChild xs)
+    where
+      isChild = (> indent x) . indent
+  assignChildren' [] = []
+
+  assignChildren :: Block -> Block
+  assignChildren b = let newChildren = assignChildren' (children b) in
+    if length newChildren == length (children b)
+      then b
+      else b { children = map assignChildren newChildren }
+
+  createRoot :: [Block] -> Block
+  createRoot blocks = Block {
+    startLine = 0, endLine = endLine (last blocks), indent = -1,
+    contents = T.pack "", children = blocks
+  }
+
+  toBlocks :: T.Text -> Block
+  toBlocks t = assignChildren . createRoot . mergeSameLevel $ zipWith lineToBlock [0..] (T.lines t)
     where
       lineToBlock i l =
         let stripped = T.dropWhile isSpace l
@@ -30,5 +57,6 @@ module Blockify where
           startLine = i,
           endLine = i,
           indent = T.length l - T.length stripped,
-          contents = stripped
+          contents = stripped,
+          children = []
         }
