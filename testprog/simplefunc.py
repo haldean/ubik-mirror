01bb636dd72c31705fdb68750135ba91eb3a4ed3
Haldean Brown <will.h.brown@gmail.com>
2016-01-07 08:58:04 -0800

    maybe kinda

---
+++ testprog/simplefunc.py
@@ -0,0 +1,14 @@
+from expelc.libexpel import *
+
+c0 = const(uint8(), t(7, 0))
+c1 = const(uint8(), t(2, 0))
+g = graph(1)
+partial = apply(func=g, arg=c0)
+res = apply(func=partial, arg=c1, terminal=True)
+g0 = [c0, c1, partial, g, res]
+
+arg0 = arg(0)
+arg1 = arg(1, terminal=True)
+g1 = [arg0, arg1]
+
+encode([g0, g1])
