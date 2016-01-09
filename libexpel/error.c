3eb15842d0617ff8f2582365e67e5f708a575c2a
Haldean Brown <will.h.brown@gmail.com>
2016-01-08 18:29:15 -0800

    add error pool

--- libexpel/error.c
+++ libexpel/error.c
@@ -23,11 +23,24 @@
 #include "expel/expel.h"
 #include "expel/util.h"
 
+#define ERROR_POOL_SIZE 64
+
+static struct xl_error error_pool[ERROR_POOL_SIZE];
+static size_t next_error;
+
 xl_error_t
 xl_new_error(word_t code, char *tag, char *file, uint32_t lineno)
 {
         struct xl_error *res;
-        res = calloc(1, sizeof(struct xl_error));
+
+        /* When we've got no memory left, we take an error from the pool we
+         * already allocated to avoid another allocation in an already
+         * memory-troubled environment. */
+        if (code == ERR_NO_MEMORY && next_error < ERROR_POOL_SIZE)
+                res = &error_pool[next_error++];
+        else
+                res = calloc(1, sizeof(struct xl_error));
+
         res->error_code = code;
         res->tag = tag;
         res->file = file;
