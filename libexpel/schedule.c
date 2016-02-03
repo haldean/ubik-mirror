9699d90507a01eaf1c95cf0d7754fccb2f56ee17
Haldean Brown <will.h.brown@gmail.com>
2016-02-03 08:50:08 -0800

    more implementation stuff

--- libexpel/schedule.c
+++ libexpel/schedule.c
@@ -63,6 +63,76 @@ xl_schedule_free(struct xl_scheduler *s)
         return OK;
 }
 
+/* Initializes the flags of a given node. */
+no_ignore static xl_error
+_set_initial_ready(struct xl_dagc_node *n)
+{
+        struct xl_dagc_node *d1, *d2, *d3;
+        xl_error err;
+
+        /* Input nodes are special; they're only ready once their values
+         * have been filled in, even though they have no dependencies.
+         * Only application of the graph they participate in can make
+         * them ready, so these don't changed here. */
+        if (n->node_type == DAGC_NODE_INPUT)
+                return OK;
+
+        err = xl_dagc_get_deps(&d1, &d2, &d3, n);
+        if (err != OK)
+                return err;
+
+        /* Cond nodes are also special; they start out only waiting on
+         * their condition, and then on the basis of their condition
+         * they are re-waited. */
+        if (n->node_type == DAGC_NODE_COND)
+        {
+                if (d1->flags & XL_DAGC_FLAG_COMPLETE)
+                        n->flags = 0;
+                else
+                        n->flags = XL_DAGC_FLAG_WAIT_D1;
+        }
+        else
+        {
+                n->flags = 0;
+                if (d1 != NULL && !(d1->flags & XL_DAGC_FLAG_COMPLETE))
+                        n->flags |= XL_DAGC_FLAG_WAIT_D1;
+                if (d2 != NULL && !(d2->flags & XL_DAGC_FLAG_COMPLETE))
+                        n->flags |= XL_DAGC_FLAG_WAIT_D2;
+                if (d3 != NULL && !(d3->flags & XL_DAGC_FLAG_COMPLETE))
+                        n->flags |= XL_DAGC_FLAG_WAIT_D3;
+        }
+
+        return OK;
+}
+
+no_ignore static xl_error
+_enqueue(
+        struct xl_scheduler *s,
+        struct xl_dagc *graph,
+        struct xl_env *env,
+        struct xl_exec_notify *notify,
+        struct xl_dagc_node *node)
+{
+        struct xl_exec_unit *u;
+        xl_error err;
+
+        u = calloc(1, sizeof(struct xl_exec_unit));
+        if (u == NULL)
+                return xl_raise(ERR_NO_MEMORY, "exec unit alloc");
+        u->node = node;
+        u->notify = notify;
+        u->env = env;
+        u->graph = graph;
+
+        err = _set_initial_ready(node);
+        if (err != OK)
+                return err;
+
+        u->next = s->wait;
+        s->wait = u;
+        return OK;
+}
+
 /* Pushes a graph into the scheduler for execution. */
 no_ignore xl_error
 xl_schedule_push(
@@ -71,12 +141,18 @@ xl_schedule_push(
         struct xl_env *env,
         struct xl_exec_notify *notify)
 {
-        unused(s);
-        unused(graph);
-        unused(env);
-        unused(notify);
+        xl_error err;
+        size_t i;
 
-        return xl_raise(ERR_NOT_IMPLEMENTED, "xl_schedule_push");
+        /* TODO: only exec reachable nodes */
+        for (i = 0; i < graph->n; i++)
+        {
+                err = _enqueue(s, graph, env, notify, graph->nodes[i]);
+                if (err != OK)
+                        return err;
+        }
+
+        return OK;
 }
 
 /* Marks an execution unit complete. */
@@ -146,6 +222,8 @@ _run_single_pass(struct xl_scheduler *s)
                 if (err != OK)
                         return err;
 
+                s->ready = s->ready->next;
+
                 if (*u->node->known.tag & TAG_GRAPH &&
                         u->node->known.graph->in_arity == 0)
                 {
@@ -157,16 +235,22 @@ _run_single_pass(struct xl_scheduler *s)
                         if (err != OK)
                                 return err;
                 }
-                else
+                else if (u->node->flags & XL_DAGC_FLAG_COMPLETE)
                 {
                         err = xl_schedule_complete(s, u);
                         if (err != OK)
                                 return err;
                 }
+                else if (u->node->flags & XL_DAGC_WAIT_MASK)
+                {
+                        u->next = s->wait;
+                        s->wait = u;
+                }
+                else return xl_raise(
+                        ERR_BAD_HEADER,
+                        "eval'ed node is not complete or waiting");
 
-                u = s->ready->next;
-                free(s->ready);
-                s->ready = u;
+                u = s->ready;
         }
 }
 
