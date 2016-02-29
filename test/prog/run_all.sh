a45cc705aa98aa4c59cb8200504d56263cba226a
Haldean Brown <will.h.brown@gmail.com>
2016-02-29 08:51:30 -0800

    add (failing) import tests

--- test/prog/run_all.sh
+++ test/prog/run_all.sh
@@ -3,6 +3,9 @@
 set -ex
 
 for xl_file in test/prog/*.xl; do
+        if grep skip-run $xl_file; then
+                continue
+        fi
         dist/expelc $xl_file a.out
         dist/runexpel a.out
         rm a.out
