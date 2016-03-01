9e4eafd34f0dcec78715fbd2518312ecbe09b90a
Haldean Brown <will.h.brown@gmail.com>
2016-02-29 22:15:36 -0800

    use relative paths in build

--- res/build-config.mk
+++ res/build-config.mk
@@ -15,8 +15,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-RES_DIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
-ROOT_DIR := $(shell dirname $(RES_DIR))
+ROOT_DIR := $(dir $(firstword $(MAKEFILE_LIST)))
 DIST_DIR := $(ROOT_DIR)/dist
 BUILD_DIR := $(ROOT_DIR)/build
 
