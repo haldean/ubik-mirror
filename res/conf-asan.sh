2feb70cbfebb87c3ead87b451f3f61b58de8799e
Haldean Brown <me@haldean.org>
2016-03-16 01:36:25 -0700

    add my common configure targets

---
+++ res/conf-asan.sh
@@ -0,0 +1,1 @@
+./configure CFLAGS='-O0 -fsanitize=address -fsanitize=undefined -ggdb -Werror -Wall -Wextra -fno-strict-aliasing' PYTHON=/bin/python2
---
+++ res/conf-rel.sh
@@ -0,0 +1,2 @@
+./configure CFLAGS='-O3 -D_FORTIFY_SOURCE=2 -Werror -Wall -Wextra -fno-strict-aliasing' PYTHON=/bin/python2
+
