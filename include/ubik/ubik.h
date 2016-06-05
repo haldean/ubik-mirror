826f901810d8ace48117a55e271f63c2f05c98af
Haldean Brown <me@haldean.org>
2016-06-05 16:00:25 -0700

    ansi c11 doesn't like __FUNCTION__ but it likes __func__

--- include/ubik/ubik.h
+++ include/ubik/ubik.h
@@ -303,6 +303,6 @@ ubik_error_explain(ubik_error err);
 
 /* Raise an error with the current file and line populated. */
 #define ubik_raise(code, tag) \
-        ubik_error_new((code), (tag), __FILE__, __LINE__, __FUNCTION__)
+        ubik_error_new((code), (tag), __FILE__, __LINE__, __func__)
 
 #define local(type) __attribute__((cleanup(ubik_ ## type ## _free)))
