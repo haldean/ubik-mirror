6799ed1de9e9dc4a9ee6c88f025d55efc6bc35df
Haldean Brown <me@haldean.org>
2016-06-05 16:00:54 -0700

    switch on -pedantic again (it's good to have you back, buddy)

--- res/build-config.mk
+++ res/build-config.mk
@@ -4,7 +4,7 @@ BISON ?= bison
 PYTHON ?= python2
 FLEX ?= flex
 
-CFLAGS := $(CFLAGS) -std=c11 -Werror -Wall -Wextra -Wswitch-enum -fno-strict-aliasing
+CFLAGS := $(CFLAGS) -std=c11 -Werror -Wall -Wextra -Wswitch-enum -fno-strict-aliasing -pedantic
 CFLAGS := $(CFLAGS) -rdynamic -I$(top)/include/ -O0 -D_GNU_SOURCE
 
 ifeq ($(careful),yes)
