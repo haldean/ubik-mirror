2f10f3d079416faf40da99e5c18bee4ce2875d5f
Haldean Brown <me@haldean.org>
2016-06-05 15:55:30 -0700

    add asan option to make

--- res/build-config.mk
+++ res/build-config.mk
@@ -7,5 +7,10 @@ FLEX ?= flex
 CFLAGS := $(CFLAGS) -std=c11 -Werror -Wall -Wextra -Wswitch-enum -fno-strict-aliasing
 CFLAGS := $(CFLAGS) -rdynamic -I$(top)/include/ -O0 -D_GNU_SOURCE
 
+ifeq ($(asan),yes)
+	CFLAGS := $(CFLAGS) -fsanitize=address -fsanitize=leak
+	CFLAGS := $(CFLAGS) -fsanitize=undefined -fstack-protector-strong
+endif
+
 LDFLAGS := $(LDFLAGS) -rdynamic
 
