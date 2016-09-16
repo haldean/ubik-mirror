08c058f7a18333f46a740959bbed8d4ad27c47a2
Haldean Brown <me@haldean.org>
2016-09-16 12:32:12 -0700

    hand off string concat to str helper

--- libubik/shared/natives/native-seq.c
+++ libubik/shared/natives/native-seq.c
@@ -19,55 +19,28 @@
 
 #include "ubik/env.h"
 #include "ubik/natives.h"
+#include "ubik/schedule.h"
+#include "ubik/str.h"
 #include "ubik/ubik.h"
 #include "ubik/util.h"
-#include "ubik/value.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 static ubik_error
-_native_concat(struct ubik_env *env, struct ubik_dagc *graph)
+_native_concat(struct ubik_exec_graph *gexec)
 {
-        ubik_error err;
         struct ubik_value *res;
-        char *str0, *str1, *concat;
-        size_t n0, n1, n;
+        ubik_error err;
 
-        unused(env);
-
-        err = ubik_string_read(&str0, &n0, graph->nodes[0]->known.tree);
-        if (err != OK)
-                return err;
-        err = ubik_string_read(&str1, &n1, graph->nodes[1]->known.tree);
+        err = ubik_value_new(&res, gexec->workspace);
         if (err != OK)
                 return err;
 
-        n = n0 + n1;
-        concat = calloc(n + 1, sizeof(char));
-        if (concat == NULL)
-                return ubik_raise(ERR_NO_MEMORY, "concat alloc");
-        memcpy(concat, str0, n0);
-        memcpy(&concat[n0], str1, n1);
-
-        err = ubik_value_new(&res);
-        if (err != OK)
-                return err;
-        err = ubik_value_pack_string(res, concat, n);
-        if (err != OK)
-                return err;
-
-        free(str0);
-        free(str1);
-        free(concat);
-
-        graph->result->known.tree = res;
-        graph->result->known_type = graph->nodes[0]->known_type;
-
-        err = ubik_take(graph->result->known_type);
-        if (err != OK)
-                return err;
+        ubik_str_concat(res, gexec->nv[0], gexec->nv[1]);
+        gexec->nv[2] = res;
+        gexec->nt[2] = gexec->nv[0];
 
         return OK;
 }
