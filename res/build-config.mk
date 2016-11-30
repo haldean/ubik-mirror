da9a263f1cfa046bdbacdd942c2c8e2cabbd4a61
Haldean Brown <me@haldean.org>
2016-11-30 00:14:56 -0800

    always turn on the stack protector

--- res/build-config.mk
+++ res/build-config.mk
@@ -5,12 +5,11 @@ PYTHON ?= python2
 FLEX ?= flex
 
 CFLAGS := $(CFLAGS) -std=c11 -Werror -Wall -Wextra -Wswitch-enum -fno-strict-aliasing -pedantic
-CFLAGS := $(CFLAGS) -ggdb -rdynamic -I$(top)/include/ -O0 -D_GNU_SOURCE
+CFLAGS := $(CFLAGS) -ggdb -rdynamic -I$(top)/include/ -O0 -D_GNU_SOURCE -fstack-protector-strong
 
 ifneq ($(CC), musl-gcc)
-ifneq ($(careful),no)
-	CFLAGS := $(CFLAGS) -fsanitize=address -fsanitize=leak
-	CFLAGS := $(CFLAGS) -fsanitize=undefined -fstack-protector-strong
+ifneq ($(sanitize),no)
+	CFLAGS := $(CFLAGS) -fsanitize=address -fsanitize=leak -fsanitize=undefined
 endif
 endif
 
