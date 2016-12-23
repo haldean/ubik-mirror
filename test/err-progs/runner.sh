795937d48c5ad9dd190e3fc6ef93d1120ed5937c
Haldean Brown <me@haldean.org>
2016-12-23 15:10:09 -0800

    fix tests to point at correct hookfile

--- test/err-progs/runner.sh
+++ test/err-progs/runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export UBIK_HOOK_ROOT=$PWD/../../hook
+export UBIK_HOOKFILE=$PWD/../../hook/hooks.txt
 export UBIK_INCLUDE=$PWD/../../lib:$PWD
 ../../bin/ubic $1 >/dev/null 2>/dev/null && exit 1
 exit 0
--- test/prog/runner.sh
+++ test/prog/runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-export UBIK_HOOK_ROOT=$PWD/../../hook
+export UBIK_HOOKFILE=$PWD/../../hook/hooks.txt
 export UBIK_INCLUDE=$PWD/../../lib:$PWD
 ../../bin/ubic $1 $1.out >/dev/null 2>/dev/null
 ../../bin/ubik $1.out >/dev/null 2>/dev/null
